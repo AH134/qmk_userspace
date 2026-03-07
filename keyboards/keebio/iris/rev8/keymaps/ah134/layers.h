d458d39bec73ea4beba81b876c9289a1d899731f
AH134 <73412379+AH134@users.noreply.github.com>
2026-03-06 16:02:22 -0800

    update rgb and add number layer

--- keyboards/keebio/iris/rev8/keymaps/ah134/layers.h
+++ keyboards/keebio/iris/rev8/keymaps/ah134/layers.h
@@ -5,14 +5,18 @@ enum layer_names {
     QWERTY,
     SYMBOL,
     NAVIGATION,
+    NUMBER,
     GAMING
 };
 
 // layer macros
 #define MO_SYM MO(SYMBOL)
 #define MO_NAV MO(NAVIGATION)
+#define MO_NUM MO(NUMBER)
 #define TG_GAM TG(GAMING)
 
 // keycode aliases
 #define ALT_TAB LALT(KC_TAB)
 #define SALT_TAB S(LALT(KC_TAB)) 
+#define LT_NUMENT LT(MO_NUM, KC_ENT) 
+#define LT_NAVENT LT(MO_NAV, KC_ENT)
